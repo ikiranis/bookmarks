6a27dafea2c4b1caf8a8c30fe078bbdaa456c76f
rocean <rocean74@gmail.com>
2018-07-18 03:38:09 +0300

    auth secrets from .env

--- src/api.js
+++ src/api.js
@@ -1,32 +1,49 @@
 import axios from 'axios';
 
-const ROOT_API = process.env.VUE_APP_ROOT_API;
+const API_HOST = process.env.VUE_APP_API_HOST;
+const ROOT_API = API_HOST + '/api';
+
 
 let api = {
 
+    /**
+     *
+     * @returns {Promise<void>}
+     */
     async getAllBookmarks() {
         let {data} = await axios.get(ROOT_API + '/bookmarks');
 
         return data;
     },
 
+    /**
+     *
+     * @param args
+     * @returns {Promise<void>}
+     */
     async saveBookmark(args) {
         let {data} = await axios.post(ROOT_API + '/bookmark', args);
 
         return data;
     },
 
+    /**
+     *
+     * @param username
+     * @param password
+     * @returns {Promise<void>}
+     */
     async login(username, password) {
         let args = {
             grant_type: "password",
-            client_id: "1",
-            client_secret: "0BsStDc0ZU5A5mXzzsEreh8JK9dbcifHsEt93xPk",
+            client_id: process.env.VUE_APP_CLIENT_ID,
+            client_secret: process.env.VUE_APP_CLIENT_SECRET,
             username: username,
             password: password,
             scope: "*"
         };
 
-        let {data} = await axios.post('http://localhost:7891/oauth/token', args);
+        let {data} = await axios.post(API_HOST + '/oauth/token', args);
 
         return data;
     }
