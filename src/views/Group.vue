31f77b88ea944970c284218546535c4765e164a9
rocean <rocean74@gmail.com>
2018-08-02 21:23:57 +0300

    Remove userId state

--- src/views/Group.vue
+++ src/views/Group.vue
@@ -5,18 +5,12 @@
 </template>
 
 <script>
-    // @ is an alias to /src
     import ManageGroup from '@/components/ManageGroup.vue';
-    import {mapState} from 'vuex';
 
     export default {
 
         components: {
             ManageGroup
-        },
-
-        computed: {
-            ...mapState(['userId'])
         }
 
     }
