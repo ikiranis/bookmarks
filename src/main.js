74676345a1797617d43c8d4aaf9a9aa717fcedd2
rocean <rocean74@gmail.com>
2018-08-06 21:08:52 +0300

    Refactor

--- src/main.js
+++ src/main.js
@@ -9,21 +9,20 @@ Vue.config.productionTip = true; // Display console warning at development mode
 
 // Bootstrap
 import BootstrapVue from 'bootstrap-vue'
-
 Vue.use(BootstrapVue);
-
-import user from '@/library/user'
-
-user.setUserTokenHeader();
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+// User Authentication
+import user from '@/library/user'
+user.setUserTokenHeader();
+
 // Set routes titles
 router.beforeEach((to, from, next) => {
-    document.title = to.meta.title
+    document.title = to.meta.title;
     next()
-})
+});
 
 new Vue({
     router,
