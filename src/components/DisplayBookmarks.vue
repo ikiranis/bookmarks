be91017ba7d2ef59816fed8f1e94857c2beb800d
rocean <rocean74@gmail.com>
2018-08-07 23:00:48 +0300

    Don't display pagination if list is empty

--- src/components/DisplayBookmarks.vue
+++ src/components/DisplayBookmarks.vue
@@ -24,7 +24,7 @@
         </div>
 
 
-        <nav aria-label="Bookmarks navigation" class="row mt-3">
+        <nav v-if="pagination.links" aria-label="Bookmarks navigation" class="row mt-3">
             <ul class="pagination ml-auto mr-auto">
                 <li class="page-item" :class="pagination.links.prev ? '' : 'disabled'">
                     <span class="page-link" v-on:click="getBookmarks(pagination.links.prev)">Previous</span>
