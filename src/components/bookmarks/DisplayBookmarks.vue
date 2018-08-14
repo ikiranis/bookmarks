21f92fd98702047de7a43b4b13ac48c9ed5a95fa
rocean <rocean74@gmail.com>
2018-08-14 18:14:49 +0300

    Search text

--- src/components/bookmarks/DisplayBookmarks.vue
+++ src/components/bookmarks/DisplayBookmarks.vue
@@ -1,13 +1,20 @@
 <template>
     <div class="displayBookmarks container">
-        <h3 class="text-center">Bookmarks</h3>
 
         <div class="row">
+            <div class="input-group col-lg-3 col-12 ml-auto mr-auto">
+                <label for="search" class="sr-only">Search</label>
+                <input type="text" max="100" class="form-control" id="search" name="search"
+                       v-model="search">
 
+                <span class="btn btn-info mx-1" v-on:click="searchText()">Search</span>
+            </div>
+        </div>
+
+        <div class="row">
             <div class="col-lg-6 col-12 mt-3" v-for="bookmark in bookmarks" :key="bookmark.id">
                 <BookmarkContent :bookmark="bookmark" :bookmarksList="true"/>
             </div>
-
         </div>
 
         <nav v-if="pagination.links" aria-label="Bookmarks navigation" class="row mt-3">
@@ -42,6 +49,7 @@
                 meta: null,
                 links: null
             },
+            search: ''
         }),
 
         props: {
@@ -70,7 +78,8 @@
             getBookmarks(page) {
                 let args = {
                     user_id: this.userId,
-                    group_id: this.groupId
+                    group_id: this.groupId,
+                    search: this.search
                 };
 
                 api.getBookmarks(args, page)
@@ -80,6 +89,10 @@
                         this.pagination.links = response.links;
                     })
                     .catch(error => console.log(error.response));
+            },
+
+            searchText() {
+                this.getBookmarks(null);
             }
 
         }
