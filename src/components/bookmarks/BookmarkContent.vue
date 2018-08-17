4b04075ddc72dc7facc4c83bf7661f7f332ab666
rocean <rocean74@gmail.com>
2018-08-17 23:41:02 +0300

    Styling

--- src/components/bookmarks/BookmarkContent.vue
+++ src/components/bookmarks/BookmarkContent.vue
@@ -1,55 +1,55 @@
 <template>
-    <div class="card col-lg-6 col-12">
-        <img v-if="bookmark.image" class="card-img-top" :src="bookmark.image" alt="Bookmark image">
+    <div class="col-lg-6 col-12 mt-3">
+        <div class="card">
+            <img v-if="bookmark.image" class="card-img-top" :src="bookmark.image" alt="Bookmark image">
 
-        <div class="card-header">
-            <div>
-                <router-link v-if="bookmarksList" :to="{ name: 'bookmark', params: { id: bookmark.id } }"
-                             :key="$route.fullPath">
-                    <strong>{{ bookmark.title }}</strong>
+            <div class="card-header">
+                <div>
+                    <router-link v-if="bookmarksList" :to="{ name: 'bookmark', params: { id: bookmark.id } }"
+                                :key="$route.fullPath">
+                        <strong>{{ bookmark.title }}</strong>
+                    </router-link>
+                    <strong v-else>{{ bookmark.title }}</strong>
+                </div>
+                <div class="text-right" v-if="bookmark.created_at">
+                    <small>Created at
+                        <span :title="moment(String(bookmark.created_at.date)).format('DD/MM/YYYY HH:mm')">
+                                {{ moment(String(bookmark.created_at.date)).format('DD/MM/YYYY') }}
+                            </span>
+                        by {{ bookmark.owner }} ;
+                    </small>
+                </div>
+            </div>
+
+            <div class="card-body">
+                <p class="card-text" v-html="bookmark.description"></p>
+
+                <a :href="bookmark.url" v-if="bookmark.url" target="new">{{ urlParser.hostname }}</a>
+
+                <div class="row mt-2">
+                    <router-link class="mx-2 px-2 bg-primary text-light"
+                                :key="tag.id" v-for="tag in bookmark.tags"
+                                :to="{ name: 'tagSearch', params: { id: tag.id } }">
+                        {{ tag.name }}
+                    </router-link>
+                </div>
+            </div>
+
+            <div class="text-right px-3" v-if="bookmark.group_name">
+                <router-link :to="{ name: 'groupSearch', params: { id: bookmark.group_id } }" :key="bookmark.group_id">
+                    {{ bookmark.group_name }}
                 </router-link>
-                <strong v-else>{{ bookmark.title }}</strong>
             </div>
-            <div class="text-right" v-if="bookmark.created_at">
-                <small>Created at
-                    <span :title="moment(String(bookmark.created_at.date)).format('DD/MM/YYYY HH:mm')">
-                            {{ moment(String(bookmark.created_at.date)).format('DD/MM/YYYY') }}
-                        </span>
-                    by {{ bookmark.owner }} ;
-                </small>
+
+            <div class="card-footer text-center" v-if="!bookmarksList && bookmark.user_id === userId">
+                <span class="btn btn-sm btn-info mx-1" v-on:click="editBookmark()">Edit</span>
+                <span class="btn btn-sm btn-danger mx-1"
+                    v-on:click="removeBookmark()">Remove</span>
             </div>
+
+            <edit-bookmark :bookmark="bookmark" v-if="isEditBookmarkOn"/>
         </div>
-
-        <div class="card-body">
-            <p class="card-text" v-html="bookmark.description"></p>
-
-            <a :href="bookmark.url" v-if="bookmark.url" target="new">{{ urlParser.hostname }}</a>
-
-            <div class="row mt-2">
-                <router-link class="mx-2 px-2 bg-primary text-light"
-                             :key="tag.id" v-for="tag in bookmark.tags"
-                             :to="{ name: 'tagSearch', params: { id: tag.id } }">
-                    {{ tag.name }}
-                </router-link>
-            </div>
-        </div>
-
-        <div class="text-right px-3" v-if="bookmark.group_name">
-            <router-link :to="{ name: 'groupSearch', params: { id: bookmark.group_id } }" :key="bookmark.group_id">
-                {{ bookmark.group_name }}
-            </router-link>
-        </div>
-
-        <div class="card-footer text-center" v-if="!bookmarksList && bookmark.user_id === userId">
-            <span class="btn btn-sm btn-info mx-1" v-on:click="editBookmark()">Edit</span>
-            <span class="btn btn-sm btn-danger mx-1"
-                  v-on:click="removeBookmark()">Remove</span>
-        </div>
-
-        <edit-bookmark :bookmark="bookmark" v-if="isEditBookmarkOn"/>
     </div>
-
-
 </template>
 
 <script>
