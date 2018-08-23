698bd55703f73e839cfc050e199cb3db84813d53
rocean <rocean74@gmail.com>
2018-08-24 00:38:04 +0300

    Split groups list to component

--- src/components/bookmarks/DisplayBookmarks.vue
+++ src/components/bookmarks/DisplayBookmarks.vue
@@ -12,22 +12,7 @@
         <div class="row">
 
             <div class="col-lg-2 col-12">
-                <div class="card my-3">
-                    <div class="card-header">
-                        <h6 class="text-center"><strong>Groups</strong></h6>
-                    </div>
-                    <div class="card-body p-0">
-                        <ul class="list-group">
-                            <li class="list-group-item list-group-item-action p-1 small" v-for="group in groups"
-                                :key="group.id">
-                                <router-link :to="{ name: 'groupSearch', params: { id: group.id } }"
-                                             :key="group.id">
-                                    {{ group.name }}
-                                </router-link>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
+                <GroupsList />
             </div>
 
             <div class="row col-lg-10 col-12 no-gutters">
@@ -58,10 +43,11 @@
     import api from '@/api';
     import BookmarkContent from "./BookmarkContent";
     import {mapState} from 'vuex';
+    import GroupsList from "../groups/GroupsList";
 
     export default {
 
-        components: {BookmarkContent},
+        components: {GroupsList, BookmarkContent},
 
         data: () => ({
             bookmarks: [],
---
+++ src/components/groups/GroupsList.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="card my-3">
+        <div class="card-header">
+            <h6 class="text-center"><strong>Groups</strong></h6>
+        </div>
+        <div class="card-body p-0">
+            <ul class="list-group">
+                <li class="list-group-item list-group-item-action p-1 small" v-for="group in groups"
+                    :key="group.id">
+                    <router-link :to="{ name: 'groupSearch', params: { id: group.id } }"
+                                 :key="group.id">
+                        {{ group.name }}
+                    </router-link>
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+    import {mapState} from 'vuex';
+
+    export default {
+
+        computed: {
+            ...mapState(['groups'])
+        }
+
+    }
+</script>
