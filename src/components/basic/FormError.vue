19828ae4acb3f8ff6661c5f34c45fbb493fd5c06
rocean <rocean74@gmail.com>
2018-08-26 00:12:37 +0300

    Form error with vue-boostrap

--- src/components/basic/FormError.vue
+++ src/components/basic/FormError.vue
@@ -1,13 +1,26 @@
 <template>
-    <div class="alert alert-danger mt-2 w-100 small">
-        {{ error }}
+    <div class="mt-2 w-100 small">
+        <b-alert variant="danger"
+                 dismissible
+                 :show="showDismissibleAlert"
+                 @dismissed="showDismissibleAlert=false">
+            {{ error }}
+        </b-alert>
     </div>
 </template>
 
 <script>
     export default {
+
+        data() {
+            return {
+                showDismissibleAlert: true
+            }
+        },
+
         props: {
             error: String
         }
+
     }
 </script>
