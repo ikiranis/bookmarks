29ea197c5d6df27e258b0d124c826af0f849ff5d
rocean <rocean74@gmail.com>
2018-09-02 18:08:50 +0300

    Remove PWA

--- src/main.js
+++ src/main.js
@@ -3,8 +3,6 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-import './registerServiceWorker'
-
 Vue.config.productionTip = true; // Display console warning at development mode
 
 // Bootstrap
