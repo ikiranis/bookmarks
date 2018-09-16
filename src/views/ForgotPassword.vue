690446e552334d352e1b48d8fbeac2a685426ef5
rocean <rocean74@gmail.com>
2018-09-17 02:10:22 +0300

    Styling

--- src/views/ForgotPassword.vue
+++ src/views/ForgotPassword.vue
@@ -1,25 +1,38 @@
 <template>
-    <div>
-        <div class="form-group row">
-            <label for="email" class="col-md-4 col-form-label text-md-right">E-Mail Address</label>
-            <div class="col-md-6">
-                <input id="email" type="email" class="form-control" v-model="email"
-                       required data-lpignore="true" readonly onfocus="this.removeAttribute('readonly');">
+    <div class="container my-3">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card card-default">
+                    <div class="card-header">Send reset token</div>
+                    <div class="card-body">
+                        <form>
+                            <div class="form-group row">
+                                <label for="email" class="col-md-4 col-form-label text-md-right">E-Mail Address</label>
+                                <div class="col-md-6">
+                                    <input id="email" type="email" class="form-control" v-model="email"
+                                           required data-lpignore="true" readonly
+                                           onfocus="this.removeAttribute('readonly');">
+                                </div>
+                            </div>
+
+                            <div class="form-group mb-3">
+                                <div class="col-md-8 mx-auto">
+                                    <button type="submit" class="btn btn-info btn-block" @click.prevent="sendToken">
+                                        Send me reset token
+                                    </button>
+                                </div>
+                            </div>
+
+                        </form>
+                    </div>
+                </div>
+
+                <display-error v-if="response.message" :response="response"/>
+
+                <loading :loading="loading"/>
+
             </div>
         </div>
-
-        <div class="form-group mb-3">
-            <div class="col-md-8 mx-auto">
-                <button type="submit" class="btn btn-info btn-block" @click.prevent="sendToken">
-                    Send me reset token
-                </button>
-            </div>
-        </div>
-
-        <display-error v-if="response.message" :response="response"/>
-
-        <loading :loading="loading" />
-
     </div>
 </template>
 
@@ -67,7 +80,7 @@
                     this.response.status = true;
 
                     this.setLoading(false);
-                } catch(error) {
+                } catch (error) {
                     this.response.message = error.response.data.message;
                     this.response.status = false;
 
